68aad67a87986245f7a2e0ca9efe64005948f398
Maciej Adamus <adamus.mac@gmail.com>
2024-01-24 12:38:11 +0100

    main

--- main2.py
+++ main2.py
@@ -1,15 +1,30 @@
 from antlr4 import *
 from grammar.dist.maciekGPLexer import maciekGPLexer
 from ProgramGenerator.AntlrToProgram import *
+from ProgramGenerator.TreeGenerator import *
+from ProgramGenerator.TreeConfig import TreeConfig
+from ProgramRunContext import ProgramRunContext
 
 
 def main():
-    lexer = maciekGPLexer(FileStream("grammar/examples/fibonacci.txt"))
-    stream = CommonTokenStream(lexer)
-    parser = maciekGPParser(stream)
-    tree = parser.program()
-    visitor = AntlrToProgram()
-    visitor.visit(tree)
+    prc = ProgramRunContext()
+    prc.input = [7, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    prc.max_executed_actions = 1000
+
+    # lexer = maciekGPLexer(FileStream("grammar/examples/max_number.txt"))
+    # stream = CommonTokenStream(lexer)
+    # parser = maciekGPParser(stream)
+    # tree = parser.program()
+    # program = AntlrToProgram()
+    # tree_xd = program.visit(tree)
+    # tree_xd.invoke(prc)
+    # print('output', prc.get_output())
+
+    config = TreeConfig()
+    program_2 = generate_program_from_config(config, 90)
+    print(program_2)
+
+    # p = generate_program_node_count(12)
 
 
 if __name__ == '__main__':
